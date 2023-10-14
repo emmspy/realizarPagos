8cbe1ba87b2f7b48d1147c3f30be393d870bec1e
emmspy <emilysky48@gmail.com>
2023-10-13 19:28:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+//funcion que me permite traer stripe y cargar la conexion a la plataforma
+import {loadStripe} from '@stripe/stripe-js'
+//componenete que me engloba otros para que tengan acceso a stripe 
+import {Elements, CardElement} from '@stripe/react-stripe-js'
+
+
+//forma de conectarme a Stripe --> clave publica 
+const stripePromise = loadStripe(
+  "pk_test_51O0aeRIn1ZsJM9qC58kM3UUFn8jHA31roz7cdYLwGS2wvEuFO1pShvwLD6TRjQkRgHvIP2DHNuHZDxqEQUykApXx00defro1bW"
+);
+//metodo que recibe un evento de chechoutForm
+const handleSubmit = (e) =>{
+  e.preventDefault();
+}
+
+const CheckoutForm = () => {
+  //el componenete de CardElement ya me valida el input 
+  return <form onSubmit={handleSubmit}>
+    <CardElement/>
+    <button>
+      comprar
+    </button>
+  </form>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //con el elements le paso la conexion a los componenetes hijos 
+    <Elements stripe={stripePromise}>
+      <CheckoutForm/>
+    </Elements>
   );
 }
 
